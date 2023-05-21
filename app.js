26040cc4bc95c7e8b989e80f08d03f630afff617
dpirvoiu <adisor.pdw@gmail.com>
2023-05-21 07:14:46 +0300

    chore: connect to DB

--- app.js
+++ app.js
@@ -1,1 +1,35 @@
-console.log('04 Store API')
+require('dotenv').config();
+
+// async errors
+const express = require('express');
+const app = express();
+const connectDB = require("./db/connect")
+
+//import
+const notFoundMiddleware = require('./middleware/not-found')
+const errorMiddleware = require('./middleware/error-handler')
+
+app.use(express.json())
+
+// routes
+app.get('/', (req,res)=>{
+  res.send('<h1>Store api</h1> <a href="/api/v1/products">products route</a>')
+})
+
+// use middleware
+
+app.use(notFoundMiddleware, errorMiddleware);
+
+const port = process.env.PORT || 3000;
+
+const start = async()=>{
+  try{
+    // connectDB
+    await connectDB(process.env.MONGO_URI)
+    app.listen(port, ()=> console.log(`server is listening to ${port}...`))
+  }catch (e) {
+    console.log(e)
+  }
+}
+
+start()
