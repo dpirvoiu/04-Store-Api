1190d33c71ea9a84b2b68143ef418bfa252ab14d
dpirvoiu <adisor.pdw@gmail.com>
2023-05-23 15:35:56 +0300

    chore: part 1

--- app.js
+++ app.js
@@ -1,35 +1,40 @@
-require('dotenv').config();
+require("dotenv").config();
 
-// async errors
-const express = require('express');
+require("express-async-errors");
+const express = require("express");
+
 const app = express();
-const connectDB = require("./db/connect")
+const connectDB = require("./db/connect");
 
-//import
-const notFoundMiddleware = require('./middleware/not-found')
-const errorMiddleware = require('./middleware/error-handler')
+const productsRouter = require("./routes/products");
 
-app.use(express.json())
+const notFoundMiddleware = require("./middleware/not-found");
+const errorMiddleware = require("./middleware/error-handler");
 
-// routes
-app.get('/', (req,res)=>{
-  res.send('<h1>Store api</h1> <a href="/api/v1/products">products route</a>')
-})
+// middleware
+app.use(express.json());
 
-// use middleware
+// ROUTES
+app.get("/", (req, res) => {
+  res.send('<h1>Store Api</h1><a href="/api/v1/products">products route</a>');
+});
 
-app.use(notFoundMiddleware, errorMiddleware);
+app.use("/api/v1/products", productsRouter);
+
+// products route
+
+app.use(notFoundMiddleware);
+app.use(errorMiddleware);
 
 const port = process.env.PORT || 3000;
 
-const start = async()=>{
-  try{
-    // connectDB
-    await connectDB(process.env.MONGO_URI)
-    app.listen(port, ()=> console.log(`server is listening to ${port}...`))
-  }catch (e) {
-    console.log(e)
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, console.log(`Server is listening to the port ${port}...`));
+  } catch (error) {
+    console.log(error);
   }
-}
+};
 
-start()
+start();
